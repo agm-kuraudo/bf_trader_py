87dd6d2e01b6c87c0a01dcd1571b32557e4b01ff
agm-kuraudo <agm12@duck.com>
2023-05-16 11:06:06 +0100

    SP-34 SP-37 SP-38 SP-39 SP-40 SP-41 SP-42 SP-43

--- api/call.py
+++ api/call.py
@@ -1,18 +1,38 @@
+import json
 import requests
-import api
-from api.api import Api
+from api.auth.auth_details import Auth
 
-class Call(Api):
+class CallException(Exception):
+    pass
+
+#TODO - The call object probably shouldnt be initialisd with the URL etc as we could have one object that makes all the calls?
+class Call():
     def __init__(self, url, http_method, auth):
         self.url = url
-        super().__init__(url, http_method)
         self.__auth = auth
 
-    def call(self, RequestBody):
+    def call(self, headers, RequestBody):
         #TODO: This should handle normal request params and JSON type messages
         #TODO - should handle all HTTP post types
         r = requests.post(headers={"X-Application": "wId8CbMYLNRjCwWm"}, url=self.url, params=RequestBody, cert=("D:/OneDrive - XHT/06_Projects/bookmaking/API_Auth/client-2048.crt", "D:/OneDrive - XHT/06_Projects/bookmaking/API_Auth/client-2048.key"))
         requests.Request
         return r.text
 
+    '''This is a special isntance of "call" design to authenticate users with a certificate and capture the login session'''
+    def callAuth(self, RequestBody):
+        try:
+            print({"X-Application": self.__auth.appKey})
+            print(self.url)
+            print(Auth.crtfile)
+            print(Auth.keyfile)
 
+            r = requests.post(headers={"X-Application": Auth.appKey}, url=self.url, params=RequestBody, cert=(Auth.crtfile, Auth.keyfile))
+            json_resp = json.loads(r.text)
+
+            if json_resp['loginStatus'] != 'SUCCESS':
+                raise Exception("Login unsuccessful! \n" + r.text)
+            else:
+                return json_resp['sessionToken']
+
+        except Exception as a:
+            raise CallException("Failed to autheticate!") from a
--- test.py
+++ test.py
@@ -1,17 +1,22 @@
-import api.auth.auth_details
-import api.auth.vault
+import traceback
+import api.auth.auth_details as bf_auth
+import api.auth.vault as bf_vault
 from api.urls import Urls
-
 from api.call import Call
+from api.request_body import RequestBody
 
 
-myAuth = api.auth.auth_details.Auth()
-myAuth.get_credentials_from_vault()
+try:
+    myAuth = bf_auth.Auth()
+    myAuth.get_credentials_from_vault()
 
-request_body={"username": myAuth.bf_userid, "password": myAuth.bf_pwd}
+    myRequestBody = RequestBody()
 
-myCall = Call(Urls.CERT_LOGIN, "post", myAuth)
+    myCall = Call(Urls.CERT_LOGIN, "post", myAuth)
 
-response = myCall.call(request_body)
+    myAuth.securityToken = myCall.callAuth(myRequestBody.populateTemplate("CertAuth", {"<USERID>": myAuth.bf_userid, "<PWD>": myAuth.bf_pwd}))
 
-print (response)
+    print (myAuth.securityToken)
+
+except bf_auth.AuthException as g:
+    print("\n".join(traceback.format_tb(g.__traceback__)))
