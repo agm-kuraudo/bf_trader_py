671700b6e1d3cfff232b75a4501daad857475823
agm-kuraudo <agm12@duck.com>
2023-05-18 16:02:08 +0100

    SP-49 - Now have a working HTTP request

--- test.py
+++ test.py
@@ -4,15 +4,23 @@ import api.auth.vault as bf_vault
 from api.urls import Urls
 from api.call import Call
 from api.request_body import RequestBody
+from api.output import Output as log
+from api.http_methods import Methods
 
-def authenticateToBetfair(myAuth):
+def authenticateToBetfair(myAuth, myCall, myRequestBody):
     try:
         myAuth.get_credentials_from_vault()
-        myRequestBody = RequestBody()
-        myCall = Call(myAuth)
         myAuth.securityToken = myCall.callAuth(myRequestBody.populateTemplate("CertAuth", {"<USERID>": myAuth.bf_userid, "<PWD>": myAuth.bf_pwd}))
     except bf_auth.AuthException as g:
-        print("\n".join(traceback.format_tb(g.__traceback__)))
+        log.log_error("\n".join(traceback.format_tb(g.__traceback__)))
 
+myRequestBody = RequestBody()
 myAuth = bf_auth.Auth()
-authenticateToBetfair(myAuth)
+myCall = Call(myAuth)
+
+
+#Step 1: Authenticate and get a Session Token!
+authenticateToBetfair(myAuth, myCall, myRequestBody)
+
+#Step 2: Extract the update to date for Event ID(s) for selected events
+myCall.call(http_method=Methods.POST, url=Urls.JSON_RPC, RequestBody=myRequestBody.getTemplate("listEvents"))
