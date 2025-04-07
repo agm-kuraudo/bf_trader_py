eaeb1897fb75b61bdee6c149503cc0206b9ea38d
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2025-04-07 14:09:25 +0100

    SP-191 - Further Changes to build postgres database and pgadmin

--- build/postgres_build.sh
+++ build/postgres_build.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # Define variables
-containerName="my_postgres"
+containerName_PG="my_postgres"
 dbPassword="$POSTGRES_PASSWORD"
 sqlFilePath="sql/create_database.sql"
 
 # Remove any existing container with the same name
-docker rm -f $containerName
+docker rm -f containerName_PG
 
 # Run the PostgreSQL container
-docker run --name $containerName --restart unless-stopped -e POSTGRES_PASSWORD=$dbPassword -p 5432:5432 -d postgres:16.1
+docker run --name containerName_PG --restart unless-stopped -e POSTGRES_PASSWORD=$dbPassword -p 5432:5432 -d postgres:16.1
 
-# Wait for the PostgreSQL server to start
-sleep 10
+# Check if PostgreSQL is ready
+until docker exec containerName_PG pg_isready -U postgres; do
+  echo "Waiting for PostgreSQL to start..."
+  sleep 2
+done
 
 # Copy the SQL file into the container
-docker cp $sqlFilePath $containerName:/docker-entrypoint-initdb.d/script.sql
+docker cp $sqlFilePath containerName_PG:/docker-entrypoint-initdb.d/script.sql
 
 # Execute the SQL file inside the container against the default 'postgres' database
-docker exec -i $containerName psql -U postgres -d postgres -f /docker-entrypoint-initdb.d/script.sql
+docker exec -i containerName_PG psql -U postgres -d postgres -f /docker-entrypoint-initdb.d/script.sql
+
+docker run --name my_pgadmin --restart unless-stopped -p 5432:5432 80:80 -d dpage/pgadmin4:latest
