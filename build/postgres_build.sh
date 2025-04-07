c3c3f1e607857d4d027c2b48602616d079e2cf53
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2025-04-07 14:02:49 +0100

    SP-191 - Changes to build postgres database

---
+++ build/postgres_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define variables
+containerName="my_postgres"
+dbPassword="$POSTGRES_PASSWORD"
+sqlFilePath="sql/create_database.sql"
+
+# Remove any existing container with the same name
+docker rm -f $containerName
+
+# Run the PostgreSQL container
+docker run --name $containerName --restart unless-stopped -e POSTGRES_PASSWORD=$dbPassword -p 5432:5432 -d postgres:16.1
+
+# Wait for the PostgreSQL server to start
+sleep 10
+
+# Copy the SQL file into the container
+docker cp $sqlFilePath $containerName:/docker-entrypoint-initdb.d/script.sql
+
+# Execute the SQL file inside the container against the default 'postgres' database
+docker exec -i $containerName psql -U postgres -d postgres -f /docker-entrypoint-initdb.d/script.sql
