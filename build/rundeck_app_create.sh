142d6b87a38c673d1af0110d61e1f43404cdf355
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2025-04-07 14:37:38 +0100

    SP-191 - Added rundeck create shell script

---
+++ build/rundeck_app_create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get the environment variable
+envVar=$(printenv vault_unseal_keys)
+
+# Get the script's directory
+scriptDir=$(dirname "$(readlink -f "$0")")
+
+echo "scriptDir: $scriptDir"
+
+# Define the relative path to the folder one level up
+localFolder=$(dirname "$scriptDir")
+
+# Build the Docker image from the Dockerfile
+docker build -t agm-karaudo/rundeck-image-01 -f "$scriptDir/rundeck_app.dockerfile" "$scriptDir"
+
+# Remove any existing container named some-rundeck
+docker rm -f some-rundeck
+
+# Replace the placeholder with the actual value
+dockerCommand="docker run --name some-rundeck --restart unless-stopped -e vault_unseal_keys='$envVar' -p 4440:4440 -v $scriptDir/run_deck_data:/home/rundeck/server/data -v $localFolder:/home/rundeck/app -e DOCKER_HOST=tcp://host.docker.internal:2375 agm-karaudo/rundeck-image-01"
+
+echo "dockerCommand: $dockerCommand"
+
+# Run the Docker command
+eval $dockerCommand
+
+# Uncomment the following line if you want to update the restart policy
+# docker update --restart unless-stopped some-rundeck
