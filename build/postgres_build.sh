9021be979f8f552539076a558ea48687e6565b48
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2025-04-07 14:12:34 +0100

    SP-191 - error fix

--- build/postgres_build.sh
+++ build/postgres_build.sh
@@ -6,10 +6,10 @@ dbPassword="$POSTGRES_PASSWORD"
 sqlFilePath="sql/create_database.sql"
 
 # Remove any existing container with the same name
-docker rm -f containerName_PG
+docker rm -f $containerName_PG
 
 # Run the PostgreSQL container
-docker run --name containerName_PG --restart unless-stopped -e POSTGRES_PASSWORD=$dbPassword -p 5432:5432 -d postgres:16.1
+docker run --name $containerName_PG --restart unless-stopped -e POSTGRES_PASSWORD=$dbPassword -p 5432:5432 -d postgres:16.1
 
 # Check if PostgreSQL is ready
 until docker exec containerName_PG pg_isready -U postgres; do
@@ -18,9 +18,9 @@ until docker exec containerName_PG pg_isready -U postgres; do
 done
 
 # Copy the SQL file into the container
-docker cp $sqlFilePath containerName_PG:/docker-entrypoint-initdb.d/script.sql
+docker cp $sqlFilePath $containerName_PG:/docker-entrypoint-initdb.d/script.sql
 
 # Execute the SQL file inside the container against the default 'postgres' database
-docker exec -i containerName_PG psql -U postgres -d postgres -f /docker-entrypoint-initdb.d/script.sql
+docker exec -i $containerName_PG psql -U postgres -d postgres -f /docker-entrypoint-initdb.d/script.sql
 
 docker run --name my_pgadmin --restart unless-stopped -p 5432:5432 80:80 -d dpage/pgadmin4:latest
