fc1564f698961f8a3eaadf783b4bec6b471418aa
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2025-04-30 15:30:27 +0100

    SP-193 - hopefully finished

--- monitor_service.py
+++ monitor_service.py
@@ -54,7 +54,7 @@ class MonitorService:
                 runner_list = json["result"][0]["runners"]
                 Log.log_debug(f"Market {market}, Runners: {len(runner_list)}")
                 runners = [runner["selectionId"] for runner in runner_list]
-                targets.append((market, status, len(runner_list), runners, target[6], target[7]))
+                targets.append((market, status, len(runner_list), runners, target[6], target[7], target[4]))
             Log.log_info(f"##############    Step 2 Complete {len(targets)} processed targets:")
             Log.log_debug(targets)
             return targets
@@ -90,6 +90,10 @@ class MonitorService:
         try:
             targets_to_update = []
             Log.log_info("##############    Step 3 get_filtered_targets")
+
+            # Initialise the nearest update time to something far in the future
+            nearest_update_time = 99999
+
             for target in open_targets:
                 Log.log_debug(target)
 
@@ -108,15 +112,19 @@ class MonitorService:
                 next_update_time = last_update_time + timedelta(seconds=seconds_until_next_update)
                 Log.log_debug(f"Next update time: {next_update_time}")
 
-                # Calculate the number of minutes until the next update is required
+                # Calculate the number of seconds until the next update is required
                 time_until_next_update = next_update_time - current_time
                 seconds_until_next_update_required = time_until_next_update.total_seconds()
                 Log.log_debug(f"Seconds until next update is required: {seconds_until_next_update_required}")
 
+                if seconds_until_next_update_required < nearest_update_time:
+                    nearest_update_time = seconds_until_next_update_required
+                    Log.log_debug(f"Nearest update time updated: {nearest_update_time}")
+
                 if target[1] == 'OPEN' and seconds_until_next_update_required < 0:
                     targets_to_update.append(target)
 
-            return targets_to_update
+            return (targets_to_update, nearest_update_time)
         except Exception as e:
             raise MonitorServiceException(f"Failed to filter targets that require update: {e}")
 
@@ -136,50 +144,107 @@ class MonitorService:
                     Log.log_debug(resp)
                     json_resp = resp.json()
                     status = json_resp["result"][0]["status"]
-                    Log.log_info(f"Market: {target[2]}, Runner: {individual_runner}, Status: {status}")
+                    Log.log_debug(f"Market: {target[2]}, Runner: {individual_runner}, Status: {status}")
                     odds = json_resp["result"][0]["runners"][0]["ex"]
-                    Log.log_info(f"odds back: {odds}")
+                    Log.log_debug(f"odds back: {odds}")
                     odds_str = json.dumps(odds) if type(odds) != dict else str(odds)
                     sql_command = ("INSERT INTO bf.market_table(\"timestamp\", market_id, runner_id, odds) VALUES (current_timestamp, %s, %s, %s);")
                     success = self.db_connection.db_write(sql_command, (target[0], individual_runner, odds_str))
-                    Log.log_debug(f"Updating odds for {target[0]} runner {individual_runner} status: {success}")
+                    Log.log_info(f"Updating odds for {target[0]} runner {individual_runner} status: {success}")
+
+                # Target[6] is the event start time
+                now = datetime.now(timezone.utc)
+                target_time = target[6]
+                next_update_time_seconds = 0
+
+                if target_time < now:
+                    Log.log_info(f"Target {target[0]} is open")
+                    next_update_time_seconds = 5
+                elif target_time < now + timedelta(hours=3):
+                    Log.log_info(f"Target {target[0]} is less than 3 hours away")
+                    next_update_time_seconds = 300
+                elif target_time < now + timedelta(hours=6):
+                    Log.log_info(f"Target {target[0]} is less than 6 hours away")
+                    next_update_time_seconds = 900
+                elif target_time < now + timedelta(hours=12):
+                    Log.log_info(f"Target {target[0]} is less than 12 hours away")
+                    next_update_time_seconds = 3600
+                else:
+                    Log.log_info(f"Target {target[0]} is more than 12 hours away")
+                    next_update_time_seconds = 14400
+
                 #Updating the last updated time for that target
-                sql_command = (f"UPDATE bf.target SET last_updated=NOW() WHERE market_id=%s;")
-                success = self.db_connection.db_write(sql_command, (target[0],))
+                sql_command = (f"UPDATE bf.target SET last_updated=NOW(), update_frequency=%s WHERE market_id=%s;")
+                success = self.db_connection.db_write(sql_command, (next_update_time_seconds, target[0],))
                 Log.log_debug(f"Updating last updated time for {target[0]} status: {success}")
+
 
         except Exception as e:
             raise MonitorServiceException(f"Failed to update runner odds: {e}")
 
     def run(self):
         try:
+
+            # reload from db needs to be true for the first iteration
+            reload_from_db = True
+
             db_details_string = self.BF.get_local_db_details()
             self.db_connection = DBOutputConnection()
             self.db_connection.open_connection(db_details_string)
+
+            # Logic to make sure only 1 instance can run at a time
+            for i in range(5):
+                start_count, finish_count = self.db_connection.db_read("SELECT SUM(CASE WHEN message = 'Monitor Service: Starting run' THEN 1 ELSE 0 END) AS starting_run_count,  SUM(CASE WHEN message = 'Monitor Service: Ending run' THEN 1 ELSE 0 END) AS ending_run_count FROM bf.log_file;")[0]
+
+                if start_count != finish_count:
+                    Log.log_warning("Monitor Service: Appears to be already running. Will retry every 60 seconds for 5 minutes")
+                    time.sleep(60)
+                else:
+                    break
+
             self.db_connection.db_write_log("Monitor Service: Starting run")
             self.authenticate_and_get_token()
 
-            # Get all of our raw target data from the database
-            raw_targets = self.get_targets()
+            for i in range(15* 60):
 
-            # Process the targets into data we can work with easily
-            targets = self.process_targets(raw_targets)
+                if reload_from_db:
+                    # Get all of our raw target data from the database
+                    raw_targets = self.get_targets()
 
-            # Update the status of targets (for example, CLOSE any markets that have closed!)
-            self.update_target_status(targets)
+                    # Process the targets into data we can work with easily
+                    targets = self.process_targets(raw_targets)
 
-            # Filter only for targets that need to be updated
+                    # Update the status of targets (for example, CLOSE any markets that have closed!)
+                    self.update_target_status(targets)
 
-            filtered_targets = self.get_filtered_targets(targets)
+                reload_from_db = False
 
-            Log.log_info(f"##############    Filtered Targets Count : {len(filtered_targets)}")
+                # Start the timer
+                start_time = time.time()
 
-            if len(filtered_targets) == 0:
-                Log.log_info("##############    No targets to update")
-            else:
-                Log.log_info("##############    Updating Targets")
-                # Updating odds for targets
-                self.update_runner_odds(filtered_targets)
+                # Filter only for targets that need to be updated
+                filtered_targets, nearest_update_seconds = self.get_filtered_targets(targets)
+
+                Log.log_info(f"##############    Filtered Targets Count : {len(filtered_targets)}. Nearest Update Time: {nearest_update_seconds}")
+
+                if len(filtered_targets) == 0:
+                    Log.log_debug("##############    No targets to update")
+                else:
+                    Log.log_info("##############    Updating Targets")
+                    # Updating odds for targets
+                    self.update_runner_odds(filtered_targets)
+                    reload_from_db = True
+
+                if nearest_update_seconds > 900:
+                    Log.log_info("##############    Next update time not within 15 minutes")
+                    break
+
+                # # Wait for the remaining time (to 1 second)
+                # elapsed_time = time.time() - start_time
+                # remaining_time = max(0.0, 1.0 - elapsed_time)
+                time.sleep(max(0.1, nearest_update_seconds - 1))
+
+            self.db_connection.db_write_log("Monitor Service: Ending run")
 
         except Exception as e:
             Log.log_error(f"Failed to update targets: {e}")
@@ -188,20 +253,7 @@ class MonitorService:
 if __name__ == "__main__":
     service = MonitorService(log_level=Log.INFO)
 
-    # Start the timer
-    start_time = time.time()
-
     # Call your service.run() function
     service.run()
 
-    # Calculate the elapsed time
-    elapsed_time = time.time() - start_time
 
-    print(elapsed_time)
-    # Calculate the remaining time to wait
-    remaining_time = max(0.0, 1.0 - elapsed_time)
-
-    print(remaining_time)
-
-    # Wait for the remaining time
-    time.sleep(remaining_time)
