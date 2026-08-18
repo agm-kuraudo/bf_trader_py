a81677012b06c9ebc3c5726d90a64b70beaccdac
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2026-08-18 16:27:07 +0100

    SP-292: remove Vault module and infrastructure files

--- build/vault_build.ps1
+++
@@ -1,5 +0,0 @@
-$jsonFilePath = "$PSScriptRoot\vault_config.json"
-docker rm -f my_keyvault
-#docker run --cap-add=IPC_LOCK --name my_keyvault -e 'VAULT_LOCAL_CONFIG={"storage": {"file": {"path": "/vault/file"}}, "listener": [{"tcp": { "address": "0.0.0.0:8200", "tls_disable": true}}], "default_lease_ttl": "168h", "max_lease_ttl": "720h", "ui": true}' -e VAULT_ADDR=http://127.0.0.1:8200 -p 8200:8200 hashicorp/vault server
-#docker run --cap-add=IPC_LOCK --name my_keyvault --restart unless-stopped --network my_trading_network --ip 172.19.0.2 -e 'VAULT_LOCAL_CONFIG={"storage": {"file": {"path": "/vault/file"}}, "listener": [{"tcp": { "address": "0.0.0.0:8200", "tls_disable": true}}], "default_lease_ttl": "168h", "max_lease_ttl": "720h", "ui": true}' -e VAULT_ADDR=http://127.0.0.1:8200 -p 8200:8200 -d hashicorp/vault server
-docker run --cap-add=IPC_LOCK --name my_vault --restart unless-stopped --network my_trading_network --ip 172.19.0.2 -v ${jsonFilePath}:/vault/config/local.json -e VAULT_ADDR=http://127.0.0.1:8200 -p 8200:8200 -d hashicorp/vault server
